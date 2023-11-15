b15c4d03c20102825019a6c5db8e201da9201575
Andrew Givens <andrewski.givens@gmail.com>
2023-11-14 22:47:27 -0700

    added Rank and styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
 
 function App() {
   return (
     <div className="App">
      
+     <ParticlesBg color='#FFFFFF' type="cobweb" bg={true} />
       <Navigation/>
       <Logo/>
-       {/* 
+      <Rank/>
       <ImageLinkForm/>
+
+             {/* 
       FaceRecognition*/}
     </div>
   );
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,9 +5,9 @@ import './Logo.css';
 
 const Logo = () => {
     return (
-        <div className="ma4 mt0">
-            <Tilt className="br2 shadow-2">
-                <div style={{ width: '1500px', height: '150px'}}>
+        <div className="ma4 mt2 parralaxLogoBackground center">
+            <Tilt className="br2 shadow-2 ">
+                <div style={{height: '150px', padding: '35px'}}>
                     <img alt="Logo" src={brain}></img>
                 </div>
             </Tilt>
